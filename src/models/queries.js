79cc4d9fe0761238bdfc59022ae8c0706fc0f9d0
Sounak Ghosh <97498294+SounakGhosh07@users.noreply.github.com>
2023-04-24 20:53:28 +0530

    Delete src directory

--- src/models/queries.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-const contact = mongoose.Schema({
-    email : String,
-    phone: String,
-    query: String
-});
-
-module.exports = mongoose.model("Query", contact)
